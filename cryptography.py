12ee8d6d2eed948b9adf6cf90241b1777a79e6d4
CriticalD20 <jamesnapier@hanovernorwichschools.org>
2015-10-23 14:53:07 -0400

    Updated from Brython Server: 10/23/2015 2:53:06 PM

--- cryptography.py
+++ cryptography.py
@@ -13,12 +13,18 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 input1=input("Enter e to encrypt, d to decrypt, or q to quit: ")
 inputr=input1.lower()
+
+
 x=1
 if x >= 0:
     if inputr == "e":
         msg_in=input("Message: ")
         key_in=input("Key: ")
-    
+        for L in msg_in:
+            associations.index(L)
+            listas=list(associations.index(L))
+                for i in listas
+                
     elif inputr == "d":
         msg_in2=("Message: ")
         key_in2=("Key: ")
