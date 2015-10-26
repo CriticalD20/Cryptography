6437e28edf2417ead2d46372330a8fe58a110766
CriticalD20 <jamesnapier@hanovernorwichschools.org>
2015-10-26 14:57:17 -0400

    Updated from Brython Server: 10/26/2015 2:57:15 PM

--- cryptography.py
+++ cryptography.py
@@ -15,22 +15,27 @@ input1=input("Enter e to encrypt, d to decrypt, or q to quit: ")
 inputr=input1.lower()
 
 
-x=1
-if x >= 0:
-    if inputr == "e":
-        msg_in=input("Message: ")
-        key_in=input("Key: ")
-        for L in msg_in:
-            associations.index(L)
-            listas=list(associations.index(L))
-                for i in listas
+if inputr == "e":
+    msg_in=input("Message: ")
+    key_in=input("Key: ")
+    key_in=key_in*(len(msg_in)//len(key_in)+1)
+    msg_a=[associations.find(x) for x in msg_in]
+    key_a=[associations.find(y) for y in key_in]
+    m_k_a=zip(msg_a, key_a)
+    comp_nums=list(z[0] + z[1] for z in m_k_a)
+    print(associations[index] for c in comp_nums)
+    #find out why there is an error in the line above/how to index from a list
+    
+        
+            
+            
                 
-    elif inputr == "d":
-        msg_in2=("Message: ")
-        key_in2=("Key: ")
+elif inputr == "d":
+    msg_in2=("Message: ")
+    key_in2=("Key: ")
      
-    elif inputr == "q":
-        print("Goodbye!")
+elif inputr == "q":
+    print("Goodbye!")
      
-    else:
-        print("Did not understand command, try again.")
+else:
+    print("Did not understand command, try again.")
