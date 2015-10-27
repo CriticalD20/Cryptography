b1b11e28ef750dbfc19bb63ac37129cbc67e39bb
CriticalD20 <jamesnapier@hanovernorwichschools.org>
2015-10-27 14:57:09 -0400

    Updated from Brython Server: 10/27/2015 2:57:09 PM

--- cryptography.py
+++ cryptography.py
@@ -23,20 +23,32 @@ if inputr == "e":
     key_a=[associations.find(y) for y in key_in]
     m_k_a=zip(msg_a, key_a)
     comp_nums=list(z[0] + z[1] for z in m_k_a)
+    for n, c in enumerate(comp_nums):
+        if c > 77:
+            c= c-78
+        else:
+            c=c
+        comp_nums[n] = c
     enclist = [associations[c] for c in comp_nums]
-    print()
-    #find out why there is an error in the line above/how to index from a list
-    
-        
+    print(''.join(enclist))
+    continue
             
-            
-                
 elif inputr == "d":
     msg_in2=("Message: ")
     key_in2=("Key: ")
-     
+    key_in=key_in*(len(msg_in)//len(key_in)+1)
+    msg_a=[associations.find(x) for x in msg_in]
+    key_a=[associations.find(y) for y in key_in]
+    m_k_a=zip(msg_a, key_a)
+    mk_a=[(t[0]+t[1]) for t in m_k_a]
+    mkz=zip(mk_a, key_a)
+    comp_nums=list(z[0] + z[1] for z in mkz)
+    enclist = [associations[z] for z in comp_nums]
+    print(''.join(enclist))
+    continue
 elif inputr == "q":
     print("Goodbye!")
      
-else:
+elif inputr != "q" and inputr != "e" and inputr !="d" and inputr !="e":
     print("Did not understand command, try again.")
+    continue
